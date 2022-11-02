de579d3e0d16b96b7829d926b17c5855ccf6feb0
adsr <adsr.qa@gmail.com>
2022-11-02 23:10:59 +0300

    [upd] patch2

--- 123.py
+++ 123.py
@@ -3,4 +3,21 @@ print(sorted(x, reverse=True))
 
 x.sort(reverse=True)
 print(x)
-#
+
+x = [int(_) for _ in input().split()]
+print(sorted(x, reverse=True))
+
+x.sort(reverse=True)
+print(x)
+
+x = [int(_) for _ in input().split()]
+print(sorted(x, reverse=True))
+
+x.sort(reverse=True)
+print(x)
+
+x = [int(_) for _ in input().split()]
+print(sorted(x, reverse=True))
+
+x.sort(reverse=True)
+print(x)
